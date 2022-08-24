9180a4bd1c2da81020e80d0b966853de81e73c49
Postograf <adventure686@mail.ru>
2022-08-25 00:31:50 +0300

    Готовая сериализация

---
+++ LinkedList.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListSerializator
+{
+    class ListNode
+    {
+        public ListNode Prev;
+        public ListNode Next;
+        public ListNode Rand; // произвольный элемент внутри списка
+        public string Data;
+    }
+
+    class ListRand
+    {
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+
+        public void Serialize(FileStream s)
+        {
+            var idByNode = new Dictionary<ListNode, int>();
+
+            //индексация элементов
+            var current = Head;
+            for (int i = 0; i < Count; i++)
+            {
+                idByNode[current] = i;
+                current = current.Next;
+            }
+
+            //запись в файл (рандомные значения записываются индексами)
+            current = Head;
+            using (var writer = new BinaryWriter(s))
+            {
+                foreach (var nodeIdPair in idByNode)
+                {
+                    writer.Write(nodeIdPair.Key.Data);
+                    writer.Write(idByNode[nodeIdPair.Key.Rand]);
+                }
+            }
+        }
+
+        public void Deserialize(FileStream s)
+        {
+            var nodes = new List<ListNode>();
+            var nodesByRandId = new Dictionary<int, List<ListNode>>();
+
+            //считывание нод
+            using (var reader = new BinaryReader(s))
+            {
+                ListNode prev = null;
+                ListNode current = null;
+
+                while (reader.PeekChar() != -1)
+                {
+                    prev = current;
+                    current = new ListNode
+                    {
+                        Prev = prev,
+                        Data = reader.ReadString()
+                    };
+
+                    if (prev != null)
+                        prev.Next = current;
+
+                    nodes.Add(current);
+                    var randId = reader.ReadInt32();
+                    if (nodesByRandId.ContainsKey(randId) == false)
+                        nodesByRandId[randId] = new List<ListNode>();
+                    nodesByRandId[randId].Add(current);
+                }
+            }
+
+            //востановление ссылок на рандомные значения
+            foreach (var nodesRandIdPair in nodesByRandId)
+            {
+                foreach (var node in nodesRandIdPair.Value)
+                {
+                    node.Rand = nodes[nodesRandIdPair.Key];
+                }
+            }
+
+            Count = nodes.Count;
+            if (Count > 0)
+            {
+                Head = nodes[0];
+                Tail = nodes[nodes.Count - 1];
+            }
+        }
+
+        public ListRand Add(string data)
+        {
+            Count++;
+
+            var newNode = new ListNode
+            {
+                Prev = Tail,
+                Data = data
+            };
+
+            if (Head == null)
+            {
+                Head = newNode;
+                Tail = Head;
+            }
+            else
+            {
+                Tail.Next = newNode;
+                Tail = newNode;
+            }
+
+            Tail.Rand = RandElement();
+
+            return this;
+        }
+
+        public ListRand AddRange(IEnumerable<string> dataList)
+        {
+            foreach (var data in dataList)
+                Add(data);
+
+            return this;
+        }
+
+        public ListNode RandElement()
+        {
+            var rand = new Random().Next(0, Count);
+            var res = Head;
+            for (int i = 0; i < rand; i++)
+            {
+                res = res.Next;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LinkedListSerializator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var path = @"Temp.txt";
+
+        var listRand = new ListRand().AddRange(
+            new string[]
+            {
+                "First",
+                "Second",
+                "Real data",
+                "NullReferenceException",
+                "[Концептуализация]"
+            }
+        );
+        var otherList = new ListRand();
+
+        using (var fileStream = new FileStream(path, FileMode.OpenOrCreate))
+        {
+            listRand.Serialize(fileStream);
+        }
+
+        using (var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+        {
+            otherList.Deserialize(fileStream);
+        }
+
+        var rand = listRand.Head;
+        var other = otherList.Head;
+        var isWork = true;
+        for (int i = 0; i < listRand.Count && isWork; i++)
+        {
+            isWork = 
+                other.Data == rand.Data 
+                && other.Prev?.Data == rand.Prev?.Data
+                && other.Next?.Data == rand.Next?.Data
+                && other.Rand?.Data == rand.Rand?.Data;
+
+            rand = rand.Next;
+            other = other.Next;
+        }
+
+        Console.WriteLine($"Is Work: {isWork}");
+        if (isWork == false)
+        {
+            Console.WriteLine();
+        }
+    }
+}
